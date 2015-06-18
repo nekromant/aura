d4efcc007bca642059aecf80183de282dbc75787
Andrew Andrianov <andrew@ncrmnt.org>
2015-06-18 10:44:39 +0300

    tests: Add susb test
    Signed-off-by: Andrew Andrianov <andrew@ncrmnt.org>

---
+++ tests/test-susb.c
@@ -0,0 +1,23 @@
+#include <aura/aura.h>
+
+
+int main() {
+	int ret; 
+	init_slog(NULL, 88);
+	struct aura_node *n = aura_open("simpleusb", "file.conf", 0x1d50, 0x6032, "www.ncrmnt.org", NULL, NULL);
+	struct aura_buffer *buf = aura_buffer_request(n, 23); 
+	aura_wait_status(n, AURA_STATUS_ONLINE);
+
+	struct aura_buffer *retbuf; 
+	ret = aura_call(n, "turnTheLedOn", &retbuf, 0x1);
+	slog(0, SLOG_DEBUG, "call ret %d", ret);
+//	aura_hexdump("Out buffer", retbuf->data, retbuf->size);
+	while(1) {
+		aura_loop_once(n);
+		sleep(1);
+	}
+	aura_close(n);
+	return 0;
+}
+
+
