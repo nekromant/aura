c2406ac3b51a329619a5c6da5b7fdd52b0c219e8
Andrew Andrianov <andrew@ncrmnt.org>
2015-06-22 17:16:39 +0300

    lua: Update aura lib
    Signed-off-by: Andrew Andrianov <andrew@ncrmnt.org>

--- lua/aura.lua
+++ lua/aura.lua
@@ -1,20 +1,5 @@
 local aura = require("auracore");
 
-function locals()
-  local variables = {}
-  local idx = 1
-  while true do
-    local ln, lv = debug.getlocal(2, idx)
-    if ln ~= nil then
-      variables[ln] = lv
-    else
-      break
-    end
-    idx = 1 + idx
-  end
-  return variables
-end
-
 aura.open = function(name, ...)
    return aura.openfuncs[name](...);
 end
