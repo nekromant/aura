d193ea54296d2f750f9e5229d8f21ac12f320f3e
Andrew Andrianov <andrew@ncrmnt.org>
2015-10-02 17:02:05 +0300

    pw-ctl-serv: Fix the script for new API
    Signed-off-by: Andrew Andrianov <andrew@ncrmnt.org>

--- pw-ctl-serv.lua
+++ pw-ctl-serv.lua
@@ -1,80 +1,110 @@
-#!/usr/bin/lua
-package.cpath=package.cpath..";./lib?.so"
-package.path=package.path..";./lua/?.lua"
+#!/usr/bin/lua5.2
 
 aura = require("aura");
-aura.slog_init(nil, 88);
+aura.slog_init(nil, 0);
 
+node = aura.open("simpleusb", "./simpleusbconfigs/iceshard.conf");
 
-node = aura.open_node("simpleusb", "./simpleusbconfigs/iceshard.conf");
-aura.wait_status(node, 1);
 
 function gpout(g,v)
    node:bit_set(bit32.lshift(g,8) + 1,1);
    node:bit_set(bit32.lshift(g,8),v);
 end
 
-gpout(0,1);
-gpout(0,1);
-
-gpout(12, 0);
-   os.execute("sleep 0.5")
-gpout(13, 0);
-   os.execute("sleep 0.5")
-gpout(14, 0);
-   os.execute("sleep 0.5")
-
-gpout(8, 0);
-   os.execute("sleep 0.5")
-gpout(9, 0);
-   os.execute("sleep 0.5")
-
-
-for i = 6100,2000,-250 do
-   node:servo(i,0)
-   os.execute("sleep 0.4")
+function gpin(g, pullup)
+   node:bit_set(bit32.lshift(g,8) + 1,0);
+   node:bit_set(bit32.lshift(g,8), pullup);
 end
 
-gpout(12, 1);
-gpout(13, 1);
-gpout(14, 1);
-gpout(8, 1);
-gpout(9, 1);
+function init()
+   -- outlets as off
+   gpout(8,1);
+   gpout(9,1);
+
+   -- relays as off 
+   gpout(12,1);
+   gpout(13,1);
+   gpout(14,1);
+
+   -- servos as Z, no pullups
+   for i = 0, 7, 1 do
+      gpin(i,0);
+   end
+
+   node:save(0,0);
+end
+
+function servo_mode(pin, on)
+   if (on) then
+      gpout(pin, 1);
+   else
+      gpin(pin, 0);
+      node:servo(pin, 0);
+   end
+end
+
+skip = 0
+for i,j in ipairs(arg) do
+   if (aura.status(node) ~= aura.STATUS_ONLINE) then
+      print("Please connect iceshard board...")
+      aura.wait_status(node, aura.STATUS_ONLINE);
+   end 
+
+   
+   if (skip > 0) then
+      skip = skip - 1;
+   elseif (j == "--init") then
+      print("Board initialized with default sane values")
+      init();
+      os.exit(0);
+   elseif (j == "--save") then
+      print("Outlet status written to EEPROM")
+      node:save(0,0);
+      os.exit(0);
+   elseif (j == "--load") then
+      print("Loaded EEPROM settings")
+      node:load(0,0);
+      os.exit(0);
+   elseif (j == "--relay") then
+      local num = tonumber(arg[i+1])
+      if not ((num > 0) and (num < 4)) then
+	 print("Invalid relay number, must be 1-3");
+	 os.exit(1);
+      end
+      local v; 
+      if (arg[i+2] == "on") then
+	 v = 0
+      else
+	 v = 1
+      end
+      skip = 2
+      gpout(15 - num, v);
+   elseif (j == "--usb") then
+      local num = tonumber(arg[i+1])
+      if not ((num > 0) and (num < 3)) then
+	 print("Invalid usb port number, must be 1-2");
+	 os.exit(1);
+      end
+      local v; 
+      if (arg[i+2] == "on") then
+	 v = 0
+      else
+	 v = 1
+      end
+      skip = 2
+      gpout(7 + num, v);
+   end
+end
+
+if #arg == 0 then
+   print("IceshardBB tool. (c) Necromant 2015");
+   print("Usage: ")
+   print("       --init             - Initialize a fresh board with default values")
+   print("       --save             - Save current state as default to EEPROM")
+   print("       --load             - Load current state from EEPROM")
+   print("       --relay N on/off   - Control relay state")
+   print("       --usb   N on/off   - Control usb port power")
+   os.exit(1);
+end
 
 os.exit(0);
-
-gpout_one(12);
-gpout_one(13);
-gpout_one(14);
-
-node:set_serial(0, 0, "sharhanas");
-node:save(0,0)
-os.exit(0);
-
-while true do
-   pcall(loop);
-end
-
---node = aura.open("gpio", 0x1d50, 0x6032, "www.ncrmnt.org");
-node = aura.open("simpleusb", "./simpleusbconfigs/pw-ctl.conf");
-
-function cb(arg) 
-   print("whoohoo");
-end
-
-node.ping = function(arg)
-   print("ping",arg);
-end
-
-evloop = aura.eventloop(node)
-
---print(node:echo_u16(34));
-
---while true do
-   evloop:handle_events(1500);
-   --end
-   evloop:handle_events(1500);
-   evloop:handle_events(1500);
-
-aura.close(node);
-
