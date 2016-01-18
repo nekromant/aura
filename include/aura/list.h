dc0cdef3d9e79e81a8b55b317d2374f4b13e9d6f
Andrew Andrianov <andrew@ncrmnt.org>
2016-01-18 12:59:55 +0300

    list.h: Do not use 'new' to make cpp compiler happy
    Signed-off-by: Andrew Andrianov <andrew@ncrmnt.org>

--- include/aura/list.h
+++ include/aura/list.h
@@ -77,40 +77,40 @@ struct list_head {
  * This is only for internal list manipulation where we know
  * the prev/next entries already!
  */
-static inline void __list_add(struct list_head *new,
+static inline void __list_add(struct list_head *neu,
 			      struct list_head *prev,
 			      struct list_head *next)
 {
-	next->prev = new;
-	new->next = next;
-	new->prev = prev;
-	prev->next = new;
+	next->prev = neu;
+	neu->next = next;
+	neu->prev = prev;
+	prev->next = neu;
 }
 
 /**
  * list_add - add a new entry
- * @new: new entry to be added
+ * @neu: new entry to be added
  * @head: list head to add it after
  *
  * Insert a new entry after the specified head.
  * This is good for implementing stacks.
  */
-static inline void list_add(struct list_head *new, struct list_head *head)
+static inline void list_add(struct list_head *neu, struct list_head *head)
 {
-	__list_add(new, head, head->next);
+	__list_add(neu, head, head->next);
 }
 
 /**
  * list_add_tail - add a new entry
- * @new: new entry to be added
+ * @neu: new entry to be added
  * @head: list head to add it before
  *
  * Insert a new entry before the specified head.
  * This is useful for implementing queues.
  */
-static inline void list_add_tail(struct list_head *new, struct list_head *head)
+static inline void list_add_tail(struct list_head *neu, struct list_head *head)
 {
-	__list_add(new, head->prev, head);
+	__list_add(neu, head->prev, head);
 }
 
 
@@ -136,8 +136,8 @@ static inline void __list_del(struct list_head * prev, struct list_head * next)
 static inline void list_del(struct list_head *entry)
 {
 	__list_del(entry->prev, entry->next);
-	entry->next = LIST_POISON1;
-	entry->prev = LIST_POISON2;
+	entry->next = (struct list_head *)LIST_POISON1;
+	entry->prev = (struct list_head *)LIST_POISON2;
 }
 
 
@@ -406,8 +406,8 @@ static inline void __hlist_del(struct hlist_node *n)
 static inline void hlist_del(struct hlist_node *n)
 {
 	__hlist_del(n);
-	n->next = LIST_POISON1;
-	n->pprev = LIST_POISON2;
+	n->next = (struct hlist_node *) LIST_POISON1;
+	n->pprev = (struct hlist_node **)LIST_POISON2;
 }
 
 
