827d628f9909a6b27dbdcf5046dd07667ba506a5
Andrew Andrianov <andrew@ncrmnt.org>
2016-03-25 20:59:59 +0300

    export.c: dbg() -> slog()
    Signed-off-by: Andrew Andrianov <andrew@ncrmnt.org>

--- export.c
+++ export.c
@@ -5,10 +5,10 @@ struct aura_export_table *aura_etable_create(struct aura_node *owner, int n)
 {
 	/* man hsearch: recommends at least 25% more to avoid collisions */
 	int nel = n + (n / 4);
-	dbg("etable: Creating etable for %d elements, %d hash entries", n, nel);
-	struct aura_export_table *tbl = calloc(1, 
-					       sizeof(struct aura_export_table) + 
-					       n * sizeof(struct aura_object)); 
+	slog(4, SLOG_DEBUG, "etable: Creating etable for %d elements, %d hash entries", n, nel);
+	struct aura_export_table *tbl = calloc(1,
+					       sizeof(struct aura_export_table) +
+					       n * sizeof(struct aura_object));
 	if (!tbl)
 		return NULL;
 
@@ -21,29 +21,29 @@ struct aura_export_table *aura_etable_create(struct aura_node *owner, int n)
 }
 
 
-void aura_etable_add(struct aura_export_table *tbl, 
-		    const char *name, 
-		    const char *argfmt, 
+void aura_etable_add(struct aura_export_table *tbl,
+		    const char *name,
+		    const char *argfmt,
 		    const char *retfmt)
 {
-	int ret; 
+	int ret;
 	ENTRY e, *ep;
 	int arg_valid = 1;
-	int ret_valid = 1; 
+	int ret_valid = 1;
 	struct aura_object *target;
 
-	if (tbl->next >= tbl->size) 
+	if (tbl->next >= tbl->size)
 		BUG(tbl->owner, "Internal BUG: Insufficient export table storage");
 
-	target = aura_etable_find(tbl, name); 
-	if (target != NULL) 
+	target = aura_etable_find(tbl, name);
+	if (target != NULL)
 		BUG(tbl->owner, "Internal BUG: Duplicate export table entry: %s", name);
 
 	target = &tbl->objects[tbl->next];
 	target->id      = tbl->next++;
 	if (!name) {
 		BUG(tbl->owner, "Internal BUG: object name can't be nil");
-	} else {  
+	} else {
 		target->name    = strdup(name);
 	}
 
@@ -51,25 +51,25 @@ void aura_etable_add(struct aura_export_table *tbl,
 		target->arg_fmt = strdup(argfmt);
 	if (retfmt)
 		target->ret_fmt = strdup(retfmt);
-	
-	if ((argfmt && !target->arg_fmt) || 
-	    (retfmt && !target->ret_fmt)) 
-		BUG(tbl->owner, "Internal allocation error"); 
-	
+
+	if ((argfmt && !target->arg_fmt) ||
+	    (retfmt && !target->ret_fmt))
+		BUG(tbl->owner, "Internal allocation error");
+
 	if (argfmt)
 		target->arg_pprinted = aura_fmt_pretty_print(argfmt, &arg_valid, &target->num_args);
 
 	if (retfmt)
 		target->ret_pprinted = aura_fmt_pretty_print(retfmt, &ret_valid, &target->num_rets);
-	
+
 	target->valid = arg_valid && ret_valid;
-	if (!target->valid) { 
-		slog(0, SLOG_WARN, "Object %d (%s) has corrupt export table", 
-		     target->id, target->name); 
+	if (!target->valid) {
+		slog(0, SLOG_WARN, "Object %d (%s) has corrupt export table",
+		     target->id, target->name);
 	}
 	/* Calculate the sizes required */
-	target->arglen = aura_fmt_len(tbl->owner, argfmt); 
-	target->retlen = aura_fmt_len(tbl->owner, retfmt); 
+	target->arglen = aura_fmt_len(tbl->owner, argfmt);
+	target->retlen = aura_fmt_len(tbl->owner, retfmt);
 
 	/* Add this shit to index */
 	e.key  = target->name;
@@ -77,13 +77,13 @@ void aura_etable_add(struct aura_export_table *tbl,
 	ret = hsearch_r(e, ENTER, &ep, &tbl->index);
 	if (!ret)
 		BUG(tbl->owner, "Internal BUG: Error adding entry to hash table");
-	
+
 }
 
-struct aura_object *aura_etable_find(struct aura_export_table *tbl, 
+struct aura_object *aura_etable_find(struct aura_export_table *tbl,
 				     const char *name)
 {
-	int ret; 
+	int ret;
 	ENTRY e, *ep;
 	struct aura_object *target = NULL;
 
@@ -91,19 +91,19 @@ struct aura_object *aura_etable_find(struct aura_export_table *tbl,
 		return NULL;
 
 	e.key  = (char *) name;
-	e.data = NULL; 
+	e.data = NULL;
 	ret = hsearch_r(e, FIND, &ep, &tbl->index);
-	if (ret) 
+	if (ret)
 		target = (struct aura_object *) ep->data;
-	return target; 	
+	return target;
 }
 
-struct aura_object *aura_etable_find_id(struct aura_export_table *tbl, 
+struct aura_object *aura_etable_find_id(struct aura_export_table *tbl,
 					int id)
 {
-	if (id >= tbl->next) 
+	if (id >= tbl->next)
 		return NULL;
-	return &tbl->objects[id];	
+	return &tbl->objects[id];
 }
 
 #define format_matches(one, two) \
@@ -145,13 +145,13 @@ static int migrate_object(struct aura_object *src, struct aura_object *dst)
 static void etable_migrate(struct aura_export_table *old, struct aura_export_table *new)
 {
 	int i;
-	struct aura_node *node; 
+	struct aura_node *node;
 	/* Sanity checking */
 	if (!old || !new) /* Nothing to migrate */
 		return;
 	if (old->owner != new->owner)
 		BUG(old->owner, "BUG during export table migration: etable owners are not equal!");
-	
+
 	node = old->owner;
 
 	/* Migration is a complex process. We need to scan tables to see if there are any
@@ -179,17 +179,17 @@ static void etable_migrate(struct aura_export_table *old, struct aura_export_tab
 		if (migrate_object(src, dst))
 			continue;
 
-		if (src->calldonecb) { 
-			/* Migration failed, object had a callback set. 
+		if (src->calldonecb) {
+			/* Migration failed, object had a callback set.
 			   We need to notify the application about a potential problem
 			 */
 			if (node->object_migration_failed_cb)
-				node->object_migration_failed_cb(node, src, 
+				node->object_migration_failed_cb(node, src,
 								 node->object_migration_failed_arg);
 			else {
-				slog(1, SLOG_WARN, 
+				slog(1, SLOG_WARN,
 				     "Migration of callbacks for object %s failed\n", src->name);
-				slog(1, SLOG_WARN, 
+				slog(1, SLOG_WARN,
 				     "Set aura_object_migration_failed_cb() callback to disable this warning\n");
 			}
 		}
@@ -198,19 +198,19 @@ static void etable_migrate(struct aura_export_table *old, struct aura_export_tab
 
 void aura_etable_activate(struct aura_export_table *tbl)
 {
-	struct aura_node *node = tbl->owner; 
+	struct aura_node *node = tbl->owner;
 
-	if (aura_get_status(node) == AURA_STATUS_ONLINE) { 
+	if (aura_get_status(node) == AURA_STATUS_ONLINE) {
 		slog(0, SLOG_FATAL, "Internal BUG: Attemped to change export table when transport is online");
 		aura_panic(node);
 	}
 
 	if (node->is_opening)
-		BUG(node, "Transport BUG: Do not call aura_etable_activate in open()");		
+		BUG(node, "Transport BUG: Do not call aura_etable_activate in open()");
 
 	if (node->etable_changed_cb)
 		node->etable_changed_cb(node, node->tbl, tbl, node->etable_changed_arg);
-	
+
 	if (node->tbl) {
 		etable_migrate(node->tbl, tbl);
 		aura_etable_destroy(node->tbl);
@@ -223,9 +223,9 @@ void aura_etable_activate(struct aura_export_table *tbl)
 void aura_etable_destroy(struct aura_export_table *tbl)
 {
 	/* Iterate over the table and free all the strings */
-	int i; 
+	int i;
 
-	for (i=0; i < tbl->next; i++) { 
+	for (i=0; i < tbl->next; i++) {
 		struct aura_object *tmp;
 		tmp=&tbl->objects[i];
 		free(tmp->name);
@@ -240,5 +240,5 @@ void aura_etable_destroy(struct aura_export_table *tbl)
 	}
 	/* Get rid of the table itself */
 	hdestroy_r(&tbl->index);
-	free(tbl);	
+	free(tbl);
 }
