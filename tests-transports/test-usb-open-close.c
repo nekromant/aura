d46c36a6a91fa96e7c0e5b7f30ca501c72d92feb
Andrew Andrianov <andrew@ncrmnt.org>
2016-04-26 23:41:08 +0300

    tests: Add open-close test for usb
    Signed-off-by: Andrew Andrianov <andrew@ncrmnt.org>

---
+++ tests-transports/test-usb-open-close.c
@@ -0,0 +1,15 @@
+#include <aura/aura.h>
+#include <unistd.h>
+int main() {
+        int ret;
+
+        slog_init(NULL, 88);
+        struct aura_node *n = aura_open("usb", "1d50:6032;www.ncrmnt.org;aura-usb-test-rig;");
+
+        if (!n) {
+                printf("err\n");
+                return -1;
+        }
+        aura_close(n);
+        return 0;
+}
