06b846cbe3dd2e4e387ea96c8f2965f54678f2ad
Andrew Andrianov <andrew@ncrmnt.org>
2016-04-27 14:31:30 +0300

    tests: Clean up warnings
    Signed-off-by: Andrew Andrianov <andrew@ncrmnt.org>

--- tests/dummy-sync-evt-free.c
+++ tests/dummy-sync-evt-free.c
@@ -5,12 +5,8 @@ int main() {
 	slog_init(NULL, 18);
 
 	int count = 5;
-	int ret;
 	struct aura_node *n = aura_open("dummy", NULL);
 	aura_wait_status(n, AURA_STATUS_ONLINE);
-
-	struct aura_buffer *retbuf;
-	const struct aura_object *o;
 
 	struct aura_eventloop *l = aura_eventloop_create(n);
 	if (!l)
