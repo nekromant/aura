5a7cbdb03cf90053c08f0164960d326337cdced5
Andrew Andrianov <andrew@ncrmnt.org>
2016-06-26 22:12:28 +0300

    tests: Update and enable usb persistence tests, minor renaming
    Signed-off-by: Andrew Andrianov <andrew@ncrmnt.org>

--- tests/xusb/susb-reconnect-persist.c
+++ tests/xusb/susb-reconnect-persist.c
@@ -1,6 +1,13 @@
 #include <aura/aura.h>
 #include <unistd.h>
 
+void run_env(const char *env)
+{
+	const char *cmd = getenv(env);
+	if (cmd) {
+		system(cmd);
+	}
+}
 
 int do_test_once(struct aura_node *n)
 {
@@ -9,13 +16,15 @@ int do_test_once(struct aura_node *n)
 	aura_wait_status(n, AURA_STATUS_ONLINE);
 	struct aura_buffer *retbuf;
 
-	ret = aura_call(n, "turnTheLedOn", &retbuf, 0x100, 0x100);
+	ret = aura_call(n, "led_ctl", &retbuf, 0x100, 0x100);
 	slog(0, SLOG_DEBUG, "call ret %d", ret);
 	if (0 != ret)
 		return 1;
 
 	aura_buffer_release(retbuf);
+	run_env("AURA_SUSB_DISCONNECT");
 	aura_wait_status(n, AURA_STATUS_OFFLINE);
+	run_env("AURA_SUSB_CONNECT");
 	return 0;
 }
 
@@ -24,11 +33,13 @@ int main()
 	int ret = 0;
 
 	slog_init(NULL, 88);
-
-	struct aura_node *n = aura_open("usb", "1d50:6032;www.ncrmnt.org;aura-usb-test-rig;");
-	if (!n)
+	struct aura_node *n = aura_open("simpleusb", "../simpleusbconfigs/susb-test.conf");
+	if (!n) {
+		printf("err when opening\n");
 		return -1;
+	}
 
+	ret += do_test_once(n);
 	ret += do_test_once(n);
 	ret += do_test_once(n);
 	ret += do_test_once(n);
