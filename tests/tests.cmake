f28441aa9c9791587fdc6fa22404b7f04b5a7148
Andrew Andrianov <andrew@ncrmnt.org>
2016-06-26 21:51:21 +0300

    tests: Run testsuite for epoll and libevent
    Signed-off-by: Andrew Andrianov <andrew@ncrmnt.org>

--- tests/tests.cmake
+++ tests/tests.cmake
@@ -1,5 +1,17 @@
 # This function adds tests from a subdirectory unders tests/
 # prefix is appended to each compiled C file
+function(add_aura_test testname testcommand)
+    set(list_var "${ARGN}")
+    ADD_TEST(epoll-${testname} ${testcommand} ${ARGN})
+    set_property(TEST epoll-${testname} PROPERTY ENVIRONMENT "AURA_USE_EVENTLOOP=epoll" "AURA_LUA_SCRIPT_PATH=${CMAKE_SOURCE_DIR}/lua")
+    set_property(TEST epoll-${testname} PROPERTY TIMEOUT 30)
+    if(LIBEVENT_FOUND)
+        ADD_TEST(libevent-${testname} ${testcommand} ${ARGN})
+        set_property(TEST libevent-${testname} PROPERTY ENVIRONMENT "AURA_USE_EVENTLOOP=libevent" "AURA_LUA_SCRIPT_PATH=${CMAKE_SOURCE_DIR}/lua")
+        set_property(TEST libevent-${testname} PROPERTY TIMEOUT 30)
+    endif()
+endfunction(add_aura_test)
+
 function(ADD_C_TEST_DIRECTORY prefix directory RUN MEMCHECK)
     file(GLOB UNITS
         "${CMAKE_SOURCE_DIR}/tests/${directory}/*.c"
@@ -19,16 +31,14 @@ function(ADD_C_TEST_DIRECTORY prefix directory RUN MEMCHECK)
       ADD_EXECUTABLE(${f} ${file})
       TARGET_LINK_LIBRARIES(${f} aurashared)
       if (${RUN})
-            ADD_TEST(${f} ${f})
-            set_property(TEST ${f} PROPERTY ENVIRONMENT "AURA_LUA_SCRIPT_PATH=${CMAKE_SOURCE_DIR}/lua")
+          add_aura_test(${f} ${f})
         if (${MEMCHECK})
-          ADD_TEST(memcheck-${f} valgrind
+          add_aura_test(memcheck-${f} valgrind
             --error-exitcode=1 --read-var-info=yes
             --leak-check=full  --show-leak-kinds=all
             --suppressions=${CMAKE_SOURCE_DIR}/valgrind.suppress
             --undef-value-errors=no --xml=yes --xml-file=${f}.xml
             ./${f})
-            set_property(TEST memcheck-${f} PROPERTY ENVIRONMENT "AURA_LUA_SCRIPT_PATH=${CMAKE_SOURCE_DIR}/lua")
         endif()
       endif()
       INSTALL(TARGETS ${f}
@@ -53,9 +63,9 @@ function(ADD_SCRIPT_TEST_DIRECTORY prefix directory ext RUN MEMCHECK)
       GET_FILENAME_COMPONENT(f ${file} NAME_WE)
       SET(f "test-${prefix}-${f}")
       if (${RUN})
-          ADD_TEST(${f} ./lua-test-wrapper ${file})
+          add_aura_test(${f} ./lua-test-wrapper ${file})
           if (${MEMCHECK})
-              ADD_TEST(memcheck-${f} valgrind
+              add_aura_test(memcheck-${f} valgrind
                   --error-exitcode=1 --read-var-info=yes
                   --leak-check=full  --show-leak-kinds=all
                   --suppressions=${CMAKE_SOURCE_DIR}/valgrind.suppress
