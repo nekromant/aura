d4774914bdcaecedc2ad43567f959a1df7cb327b
Andrew Andrianov <andrew@ncrmnt.org>
2016-09-09 17:45:09 +0300

    CMakeLists.txt: Rework test scripts
    Signed-off-by: Andrew Andrianov <andrew@ncrmnt.org>

--- tests/tests.cmake
+++ tests/tests.cmake
@@ -1,22 +1,45 @@
 # This function adds tests from a subdirectory unders tests/
 # prefix is appended to each compiled C file
-function(add_aura_test testname testcommand)
-    set(list_var "${ARGN}")
 
+
+function(add_test_with_valgrind testname testtimeout testenv testcommand memcheck testargs)
     # If we run tests on remote hardware, rewrite the testcommand to invoke
     # ssh. The remote should have passwordless auth
+
+    message("test ${testname} timeout ${testtimeout} cmd ${testcommand}")
+
     if (AURA_TEST_REMOTE)
         SET(testcommand ssh ${AURA_TEST_REMOTE_HOST} ${testcommand})
     endif()
 
+    set(VALGRIND_OPTS --error-exitcode=1 --read-var-info=yes
+          --leak-check=full ${AURA_EXTRA_VALGRIND_OPTS}
+          --suppressions=valgrind.suppress
+          --undef-value-errors=no --xml=yes --xml-file=${testname}.xml
+    )
+
+    ADD_TEST(${testname} ${testcommand} ${testargs})
+    set_property(TEST ${testname} PROPERTY ENVIRONMENT ${testenv})
+    set_property(TEST ${testname} PROPERTY TIMEOUT "${testtimeout}")
+
+    if (false)
+        ADD_TEST(memcheck-${testname} valgrind ${VALGRIND_OPTS} ${testcommand})
+        set_property(TEST memcheck-${testname} PROPERTY ENVIRONMENT ${testenv})
+        set_property(TEST memcheck-${testname} PROPERTY TIMEOUT ${testtimeout})
+    endif()
+endfunction()
+
+
+function(add_aura_test testname memcheck testcommand testargs)
     SET(testname "${AURA_TEST_PREFIX}-${testname}")
-    ADD_TEST(epoll-${testname} ${testcommand} ${ARGN})
-    set_property(TEST epoll-${testname} PROPERTY ENVIRONMENT ${CTEST_ENVIRONMENT} "AURA_USE_EVENTLOOP=epoll" "AURA_LUA_SCRIPT_PATH=${CMAKE_SOURCE_DIR}/lua")
-    set_property(TEST epoll-${testname} PROPERTY TIMEOUT ${AURA_TEST_TIMEOUT})
+    message(${testcommand})
+
+    SET(testenv ${CTEST_ENVIRONMENT} "AURA_USE_EVENTLOOP=epoll" "AURA_LUA_SCRIPT_PATH=${CMAKE_SOURCE_DIR}/lua")
+    add_test_with_valgrind(epoll-${testname} "${AURA_TEST_TIMEOUT}" "${testenv}" "${testcommand}" "${memcheck}" "${testargs}")
+
     if(LIBEVENT_FOUND)
-        ADD_TEST(libevent-${testname} ${testcommand} ${ARGN})
-        set_property(TEST libevent-${testname} PROPERTY ENVIRONMENT ${CTEST_ENVIRONMENT} "AURA_USE_EVENTLOOP=libevent" "AURA_LUA_SCRIPT_PATH=${CMAKE_SOURCE_DIR}/lua")
-        set_property(TEST libevent-${testname} PROPERTY TIMEOUT ${AURA_TEST_TIMEOUT})
+        SET(testenv ${CTEST_ENVIRONMENT} "AURA_USE_EVENTLOOP=epoll" "AURA_LUA_SCRIPT_PATH=${CMAKE_SOURCE_DIR}/lua")
+        add_test_with_valgrind(libevent-${testname} ${AURA_TEST_TIMEOUT} "${testenv}" "${testcommand}" "${memcheck}" "${testargs}")
     endif()
 endfunction(add_aura_test)
 
@@ -27,7 +50,7 @@ function(ADD_C_TEST_DIRECTORY prefix directory RUN MEMCHECK)
 
     if (${RUN})
         SET(AURA_TEST_SUITES_TO_RUN "${AURA_TEST_SUITES_TO_RUN}${prefix} " PARENT_SCOPE)
-        if (${MEMCHECK})
+        if (MEMCHECK)
             SET(AURA_TEST_SUITES_TO_RUN "${AURA_TEST_SUITES_TO_RUN}${prefix}(+memcheck) " PARENT_SCOPE)
         endif()
     endif()
@@ -39,15 +62,7 @@ function(ADD_C_TEST_DIRECTORY prefix directory RUN MEMCHECK)
       ADD_EXECUTABLE(${f} ${file})
       TARGET_LINK_LIBRARIES(${f} aurashared)
       if (${RUN})
-          add_aura_test(${f} ./${f})
-        if (${MEMCHECK})
-          add_aura_test(memcheck-${f} valgrind
-            --error-exitcode=1 --read-var-info=yes
-            --leak-check=full ${AURA_EXTRA_VALGRIND_OPTS}
-            --suppressions=${CMAKE_SOURCE_DIR}/valgrind.suppress
-            --undef-value-errors=no --xml=yes --xml-file=${f}.xml
-            ./${f})
-        endif()
+          add_aura_test(${f} ${MEMCHECK} ./${f} "")
       endif()
       INSTALL(TARGETS ${f}
         RUNTIME DESTINATION lib/${CMAKE_LIBRARY_PATH}/aura/tests/)
@@ -67,19 +82,12 @@ function(ADD_SCRIPT_TEST_DIRECTORY prefix directory ext RUN MEMCHECK)
             SET(AURA_TEST_SUITES_TO_RUN "${AURA_TEST_SUITES_TO_RUN}${prefix}(+memcheck) " PARENT_SCOPE)
         endif()
     endif()
+
     foreach(file ${UNITS})
       GET_FILENAME_COMPONENT(f ${file} NAME_WE)
       SET(f "test-${prefix}-${f}")
       if (${RUN})
-          add_aura_test(${f} ./lua-test-wrapper ${file})
-          if (${MEMCHECK})
-              add_aura_test(memcheck-${f} valgrind
-                  --error-exitcode=1 --read-var-info=yes
-                  --leak-check=full ${AURA_EXTRA_VALGRIND_OPTS}
-                  --suppressions=${CMAKE_SOURCE_DIR}/valgrind.suppress
-                  --undef-value-errors=no --xml=yes --xml-file=${f}.xml
-              ./lua-test-wrapper ${file})
-          endif()
+          add_aura_test(${f} "${MEMCHECK}" "./lua-test-wrapper" ${file})
       endif()
     endforeach(file)
 endfunction(ADD_SCRIPT_TEST_DIRECTORY)
@@ -87,3 +95,4 @@ endfunction(ADD_SCRIPT_TEST_DIRECTORY)
 
 file(WRITE ${CMAKE_BINARY_DIR}/.bashrc "export AURA_BUILDDIR=${CMAKE_BINARY_DIR}\n")
 file(APPEND ${CMAKE_BINARY_DIR}/.bashrc "export AURA_SOURCEDIR=${CMAKE_SOURCE_DIR}\n")
+file(COPY ${CMAKE_SOURCE_DIR}/valgrind.suppress DESTINATION ${CMAKE_BINARY_DIR})
