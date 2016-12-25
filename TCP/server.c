d556407706e55adc15e7320582beab703acb89ae
Andrew Andrianov <andrew@ncrmnt.org>
2016-12-25 23:56:02 +0300

    Add async io demo
    Signed-off-by: Andrew Andrianov <andrew@ncrmnt.org>

--- TCP/server.c
+++ TCP/server.c
@@ -1,79 +1,72 @@
 /*
-	C socket server example
-*/
+ *      C socket server example
+ */
 
-#include<stdio.h>
-#include<string.h>	//strlen
-#include<sys/socket.h>
-#include<arpa/inet.h>	//inet_addr
-#include<unistd.h>	//write
+#include <stdio.h>
+#include <string.h>             //strlen
+#include <sys/socket.h>
+#include <arpa/inet.h>          //inet_addr
+#include <unistd.h>             //write
 
-char shit[8192]="blah\n\r\n\r";
+char shit[8192] = "blah\n\r\n\r";
 
-int main(int argc , char *argv[])
+int main(int argc, char *argv[])
 {
 	int socket_desc, client_sock, c, read_size;
 	struct sockaddr_in server, client;
 	char client_message[2000];
-	
+
 	//Create socket
-	socket_desc = socket(AF_INET , SOCK_STREAM , 0);
+	socket_desc = socket(AF_INET, SOCK_STREAM, 0);
 	if (socket_desc == -1)
-	{
 		printf("Could not create socket");
-	}
 	puts("Socket created");
-	
+
 	//Prepare the sockaddr_in structure
 	server.sin_family = AF_INET;
 	server.sin_addr.s_addr = INADDR_ANY;
-	server.sin_port = htons( 8888 );
-	
+	server.sin_port = htons(8888);
+
 	//Bind
-	if( bind(socket_desc,(struct sockaddr *)&server , sizeof(server)) < 0)
-	{
+	if (bind(socket_desc, (struct sockaddr *)&server, sizeof(server)) < 0) {
 		//print the error message
 		perror("bind failed. Error");
 		return 1;
 	}
 	puts("bind done");
-	
+
 	//Listen
-	listen(socket_desc , 3);
-	
+	listen(socket_desc, 3);
+
 	//Accept and incoming connection
 	puts("Waiting for incoming connections...");
 	c = sizeof(struct sockaddr_in);
-	
-	//accept connection from an incoming client
-	client_sock = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c);
-	if (client_sock < 0)
-	{
-		perror("accept failed");
-		return 1;
+
+
+	while (1) {
+		printf("Waiting for a new client\n");
+		//accept connection from an incoming client
+		client_sock = accept(socket_desc, (struct sockaddr *)&client, (socklen_t *)&c);
+		if (client_sock < 0) {
+			perror("accept failed");
+			return 1;
+		}
+		puts("Connection accepted");
+
+
+		while (1) {
+			int read_size = recv(client_sock, client_message, 2, 0);
+			printf("%d bytes read\n", read_size);
+			int wr_size = write(client_sock, shit, read_size);
+			printf("%d bytes sent back!\n", wr_size);
+			if (read_size == 0) {
+				printf("Client disconnected, bailing out\n");
+				break;
+			close(client_sock);
+			break;
+			}
+		}
 	}
-	puts("Connection accepted");
-	
-	//Receive a message from client
-	while( (read_size = recv(client_sock , client_message , 2000 , 0)) > 0 )
-	{
-		//Send the message back to client
-		write(client_sock , shit , 8192);
-		printf("message get! %s\n", client_message);
-		fsync(client_sock);
-		//sleep(100);
-		//write(client_sock, "I'm here!", strlen("I'm here!"));
-	}
-	
-	if(read_size == 0)
-	{
-		puts("Client disconnected");
-		fflush(stdout);
-	}
-	else if(read_size == -1)
-	{
-		perror("recv failed");
-	}
-	
+
 	return 0;
 }
--- src/transports/transport-uart.c
+++ src/transports/transport-uart.c
@@ -2,16 +2,17 @@
 #include <aura/private.h>
 #include <aura/packetizer.h>
 #include <aura/timer.h>
-#include<sys/socket.h> 
-#include<arpa/inet.h> 
- 
+#include<sys/socket.h>
+#include <arpa/inet.h>
+#include <sys/epoll.h>
+
 
 #define CB_ARG (void *) 0xdeadf00d
 
 struct uart_dev_info {
 	int descr;
 	struct sockaddr_in server;
-};	
+};
 
 static void uart_populate_etable(struct aura_node *node)
 {
@@ -44,6 +45,25 @@ static void online_cb_fn(struct aura_node *node,  struct aura_timer *tm, void *a
 	aura_set_status(node, AURA_STATUS_ONLINE);
 }
 
+
+#include <fcntl.h>
+
+/** Returns true on success, or false if there was an error */
+bool SetSocketBlockingEnabled(int fd, bool blocking)
+{
+   if (fd < 0) return false;
+
+#ifdef WIN32
+   unsigned long mode = blocking ? 0 : 1;
+   return (ioctlsocket(fd, FIONBIO, &mode) == 0) ? true : false;
+#else
+   int flags = fcntl(fd, F_GETFL, 0);
+   if (flags < 0) return false;
+   flags = blocking ? (flags&~O_NONBLOCK) : (flags|O_NONBLOCK);
+   return (fcntl(fd, F_SETFL, flags) == 0) ? true : false;
+#endif
+}
+
 static int uart_open(struct aura_node *node, const char *opts)
 {
 	struct uart_dev_info *inf = calloc(1, sizeof(*inf));
@@ -52,7 +72,7 @@ static int uart_open(struct aura_node *node, const char *opts)
 
 
 	slog(1, SLOG_INFO, "Opening uart transport");
-     
+
 	//Create socket
 	inf->descr = socket(AF_INET , SOCK_STREAM, 0);
 	if (inf->descr == -1)
@@ -60,18 +80,18 @@ static int uart_open(struct aura_node *node, const char *opts)
 		slog(1, SLOG_ERROR, "Could not create socket");
 	}
        	slog(3, SLOG_INFO, "Socket created");
-     
+
     	inf->server.sin_addr.s_addr = inet_addr("127.0.0.1");
     	inf->server.sin_family = AF_INET;
     	inf->server.sin_port = htons( 8888 );
- 
+
     	//Connect to remote server
     	if (connect(inf->descr , (struct sockaddr *)&inf->server , sizeof(inf->server)) < 0)
     	{
         	slog(1, SLOG_ERROR, "connect failed. Error");
         	return 1;
    	}
-     
+
     	slog(3, SLOG_INFO, "Connected");
 
 	aura_set_transportdata(node, inf);
@@ -80,7 +100,8 @@ static int uart_open(struct aura_node *node, const char *opts)
 	tv.tv_sec = 0;
 	tv.tv_usec = 1;
 	aura_timer_start(online, AURA_TIMER_FREE, &tv);
-
+	SetSocketBlockingEnabled(inf->descr, 1);
+	aura_add_pollfds(node, inf->descr, EPOLLIN | EPOLLPRI | EPOLLRDHUP);
 	return 0;
 }
 
@@ -96,38 +117,39 @@ static void uart_handle_event(struct aura_node *node, enum node_event evt, const
 	struct aura_buffer *buf;
 	struct uart_dev_info *inf = aura_get_transportdata(node);
 	char str[2000] = "";
+	slog(0, SLOG_DEBUG, "Node event: %d\n", evt);
 
-
-	while (1) {
+	if (evt == NODE_EVENT_HAVE_OUTBOUND) {
 		buf = aura_node_read(node);
-	
-		if (!buf)
-			break;
+		/* TODO: send/write may deliver only a few bytes here */
+		int wrlen = write(inf->descr, buf->data, aura_buffer_get_length(buf));
+		slog(0, SLOG_DEBUG, "%d/%d bytes written", wrlen, aura_buffer_get_length(buf));
+		if (wrlen != aura_buffer_get_length(buf))
+			BUG(node, "NOT IMPLEMENTED");
+		aura_buffer_release(buf);
+		fsync(inf->descr);
+	}
 
-		slog(0, SLOG_DEBUG, "uart: serializing buf data %x len 0x%x", buf->data, aura_buffer_get_length(buf));
-
-        	if (send(inf->descr, buf->data, aura_buffer_get_length(buf), MSG_DONTWAIT) < 0)
-        	{
-            		slog(1, SLOG_ERROR, "Send failed");
-        	}
-
-	        slog(0, SLOG_DEBUG, "Send done \n");
-		
-		int ret = read(inf->descr, str, 2);
-		slog(1, SLOG_INFO, "read %d bytes", ret);
-
-		if(recv(inf->descr, str, 2 , MSG_DONTWAIT) < 0)
-		{
-			slog(1, SLOG_ERROR, "read failed");
+	if (fd) {
+		slog(0, SLOG_DEBUG, "Socket event");
+		if (fd->events & EPOLLIN) {
+			slog(0, SLOG_DEBUG, "We can read some shit!");
+			buf = aura_buffer_request(node, 128);
+			/* FixMe: We should get the id from remote side */
+			buf->object = aura_etable_find(node->tbl, "echo_u16");
+			int rdlen = read(inf->descr, buf->data, 128);
+			buf->payload_size = rdlen;
+			slog(0, SLOG_DEBUG, "Read %d bytes, assuming echo_u16", rdlen);
+			aura_node_write(node, buf);
 		}
 
-		slog(1, SLOG_INFO, str);
-
-		if (buf->object != NULL)
-			printf("s1='%d'\n", (uint16_t) &buf->data[0]);
-
-		aura_node_write(node, buf);
+		if (fd->events & EPOLLRDHUP) {
+			slog(0, SLOG_DEBUG, "Disconnected from server");
+			aura_set_status(node, AURA_STATUS_OFFLINE);
+		}
 	}
+
+
 }
 
 static struct aura_transport uart = {
